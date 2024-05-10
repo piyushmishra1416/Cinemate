9d5e773eccb2a2c0655f3f5e056973c9a431b5d6
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-05-10 12:39:34 +0530

    added the starting page and assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,38 @@
-import { Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
+import { Image, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import 'react-native-gesture-handler';
+
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-gesture-handler";
+import { images } from "../constants";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Aora!</Text>
-      <StatusBar style='auto' />
-      <Link href="/profile" style = {{color: 'blue'}}>Go to Profile</Link>
-    </View>
-  )
-}
+    <GestureHandlerRootView>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          /> 
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+};
 
-export default App
+export default App;
 
 // const styles = StyleSheet.create({
 //   container: {
