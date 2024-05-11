d9b6f4550da3472568238bccce9e6e2d8069f5e2
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-05-11 11:23:54 +0530

    starting page completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,18 @@
-import { Image, Text, View } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
-import 'react-native-gesture-handler';
+import { Redirect, router } from "expo-router";
+import "react-native-gesture-handler";
 
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView } from "react-native-gesture-handler";
 import { images } from "../constants";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import CustomButton from "@/components/CustomButton";
 
 const App = () => {
   return (
-    <GestureHandlerRootView>
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+      <ScrollView contentContainerStyle={{ }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -24,26 +22,35 @@ const App = () => {
             source={images.cards}
             className="max-w-[380px] w-full h-[300px]"
             resizeMode="contain"
-          /> 
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
-    </GestureHandlerRootView>
   );
 };
 
 export default App;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     display:'flex',
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 20,
-//   },
-//   link: {
-//     marginTop: 15,
-//     paddingVertical: 15,
-//   },
-// });
